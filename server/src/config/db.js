482d0f59d3c9769892b4bf9346d955fb28af17e5
niyokuru2 <niyonkurusam5@gmail.com>
2026-04-25 19:02:01 +0200

    open

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,6 +1,5 @@
 import sqlite3 from "sqlite3";
-//import { open } from "sqlite";
-
+import { open } from "sqlite";
 export const initDB = async () => {
   const db = await open({
     filename: "./database.sqlite",
